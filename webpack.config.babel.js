1cb7547c8c731f1656254c7255685588358f48f9
builden <billdeng@qq.com>
2015-11-18 14:38:52 +0800

    initial commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,42 @@
+import webpack from 'webpack';
+
+const config = {
+  entry: {
+    app: __dirname + '/lib/app.js',
+    vendors: ['lodash', 'moment', 'underscore.string', 'redux', 'redux-thunk', 'redux-logger'],
+  },
+
+  output: {
+    path: __dirname + '/src',
+    publicPath: '/src/', // for webpack-dev-server
+    filename: '[name].js',
+  },
+
+  devtool: 'cheap-module-source-map',
+
+  // 防止重复解析
+  resolve: {
+    alias: {},
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.(js)$/, exclude: /node_modules/,
+        loader: 'babel?stage=0&optional=runtime',
+      },
+      {
+        test: /\.(json)/, loader: 'json',
+      },
+    ],
+    // 防止重复去解析
+    noParse: [],
+  },
+
+  plugins: [
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+  ],
+};
+
+export default config;
